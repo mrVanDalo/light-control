0b145fa6abac8b5312539333c44cdc31b1085c46
Ingolf Wagner <contact@ingolf-wagner.de>
2020-05-26 22:21:48 +0200

    feat: using threads

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,8 +1,15 @@
+extern crate mustache;
+
 use self::mustache::MapBuilder;
 use serde_json::Value;
 use std::collections::HashSet;
 
-extern crate mustache;
+/// Room setup
+#[derive(Clone)]
+pub struct Configuration {
+    pub sensors: Vec<Sensor>,
+    pub switches: Vec<Switch>,
+}
 
 impl Configuration {
     pub fn update_switch(&mut self, topic: &str, state: SwitchState) {
@@ -156,14 +163,9 @@ impl Configuration {
     }
 }
 
-/// Room setup
-pub struct Configuration {
-    pub sensors: Vec<Sensor>,
-    pub switches: Vec<Switch>,
-}
-
 /// A Sensor is a device that generates inputs
 /// like door open/close or motion detected undetected
+#[derive(Clone)]
 pub struct Sensor {
     /// topic to listen to
     pub topic: String,
@@ -179,107 +181,7 @@ pub struct Sensor {
     pub presents_negator: bool,
 }
 
-/// A Switch is a device that receives commands
-/// like lights on/off
-pub struct Switch {
-    /// uniq topic to listen for the switch
-    pub topic: String,
-    /// key for state
-    pub key: String,
-    /// rooms this switch is placed
-    pub rooms: Vec<String>,
-    /// state of the switch
-    pub state: SwitchState,
-    /// command control
-    pub command: SwitchCommand,
-}
-
-impl Switch {
-    pub fn get_topic_and_command(&self, state: SwitchState, brightness: u8) -> (&String, String) {
-        self.command.get_topic_and_command(state, brightness)
-    }
-}
-
-pub struct SwitchCommand {
-    /// turn on and off command
-    /// This is a mustache template. The arguments given are
-    /// * state : on/off (see on off statement)
-    /// * brightness : 0 - 255
-    pub command: String,
-    /// command to get state of the device
-    /// useful at program start.
-    pub init_command: Option<String>,
-    /// topic to send the command under
-    pub topic: String,
-    /// string to send for state argument to run switch on
-    pub on: String,
-    /// string to send for state argument to run switch off
-    pub off: String,
-}
-
-#[derive(Debug)]
-pub enum SwitchState {
-    On,
-    Off,
-}
-
-impl SwitchCommand {
-    pub fn get_topic_and_command(&self, state: SwitchState, brightness: u8) -> (&String, String) {
-        let state_value = match state {
-            SwitchState::On => &self.on,
-            SwitchState::Off => &self.off,
-        };
-        let data = MapBuilder::new()
-            .insert("state", state_value)
-            .unwrap()
-            .insert("brightness", &brightness.to_string())
-            .unwrap()
-            .build();
-        let topic = &self.topic;
-        let template = mustache::compile_str(&self.command).expect("couldn't create template ");
-        let command = template.render_data_to_string(&data).unwrap();
-        (&topic, command)
-    }
-}
-
-#[cfg(test)]
-mod switch_tests {
-    use super::*;
-
-    #[test]
-    fn test_get_topic_and_command() {
-        let switch_command = SwitchCommand {
-            command: r#"{"test":{{state}}}"#.to_string(),
-            topic: "test/test/test".to_string(),
-            on: "1".to_string(),
-            off: "0".to_string(),
-        };
-        let (topic, command) = switch_command.get_topic_and_command(SwitchState::On, 123);
-        assert_eq!(topic, "test/test/test");
-        assert_eq!(command, r#"{"test":1}"#);
-        let (topic, command) = switch_command.get_topic_and_command(SwitchState::Off, 123);
-        assert_eq!(topic, "test/test/test");
-        assert_eq!(command, r#"{"test":0}"#);
-    }
-
-    #[test]
-    fn test_get_topic_and_command2() {
-        let switch_command = SwitchCommand {
-            command: r#"{"state":"{{state}}","brightness":{{brightness}}}"#.to_string(),
-            topic: "lights/light_1/set".to_string(),
-            on: "ON".to_string(),
-            off: "OFF".to_string(),
-        };
-        let (topic, command) = switch_command.get_topic_and_command(SwitchState::On, 123);
-        assert_eq!(topic, "lights/light_1/set");
-        assert_eq!(command, r#"{"state":"ON","brightness":123}"#);
-        let (topic, command) = switch_command.get_topic_and_command(SwitchState::Off, 123);
-        assert_eq!(topic, "lights/light_1/set");
-        assert_eq!(command, r#"{"state":"OFF","brightness":123}"#);
-    }
-}
-
-#[derive(Debug, Eq, PartialEq)]
+#[derive(Debug, Eq, PartialEq, Clone, Copy)]
 pub enum SensorState {
     /// Presents is detected
     Present,
@@ -335,5 +237,109 @@ impl SensorState {
             }
             _ => None,
         }
+    }
+}
+
+/// A Switch is a device that receives commands
+/// like lights on/off
+#[derive(Clone)]
+pub struct Switch {
+    /// uniq topic to listen for the switch
+    pub topic: String,
+    /// key for state
+    pub key: String,
+    /// rooms this switch is placed
+    pub rooms: Vec<String>,
+    /// state of the switch
+    pub state: SwitchState,
+    /// command control
+    pub command: SwitchCommand,
+}
+
+impl Switch {
+    pub fn get_topic_and_command(&self, state: SwitchState, brightness: u8) -> (&String, String) {
+        self.command.get_topic_and_command(state, brightness)
+    }
+}
+
+#[derive(Clone)]
+pub struct SwitchCommand {
+    /// turn on and off command
+    /// This is a mustache template. The arguments given are
+    /// * state : on/off (see on off statement)
+    /// * brightness : 0 - 255
+    pub command: String,
+    /// command to get state of the device
+    /// useful at program start.
+    pub init_command: Option<String>,
+    /// topic to send the command under
+    pub topic: String,
+    /// string to send for state argument to run switch on
+    pub on: String,
+    /// string to send for state argument to run switch off
+    pub off: String,
+}
+
+#[derive(Debug, Copy, Clone)]
+pub enum SwitchState {
+    On,
+    Off,
+}
+
+impl SwitchCommand {
+    pub fn get_topic_and_command(&self, state: SwitchState, brightness: u8) -> (&String, String) {
+        let state_value = match state {
+            SwitchState::On => &self.on,
+            SwitchState::Off => &self.off,
+        };
+        let data = MapBuilder::new()
+            .insert("state", state_value)
+            .unwrap()
+            .insert("brightness", &brightness.to_string())
+            .unwrap()
+            .build();
+        let topic = &self.topic;
+        let template = mustache::compile_str(&self.command).expect("couldn't create template ");
+        let command = template.render_data_to_string(&data).unwrap();
+        (&topic, command)
+    }
+}
+
+#[cfg(test)]
+mod switch_tests {
+    use super::*;
+
+    #[test]
+    fn test_get_topic_and_command() {
+        let switch_command = SwitchCommand {
+            command: r#"{"test":{{state}}}"#.to_string(),
+            init_command: None,
+            topic: "test/test/test".to_string(),
+            on: "1".to_string(),
+            off: "0".to_string(),
+        };
+        let (topic, command) = switch_command.get_topic_and_command(SwitchState::On, 123);
+        assert_eq!(topic, "test/test/test");
+        assert_eq!(command, r#"{"test":1}"#);
+        let (topic, command) = switch_command.get_topic_and_command(SwitchState::Off, 123);
+        assert_eq!(topic, "test/test/test");
+        assert_eq!(command, r#"{"test":0}"#);
+    }
+
+    #[test]
+    fn test_get_topic_and_command2() {
+        let switch_command = SwitchCommand {
+            command: r#"{"state":"{{state}}","brightness":{{brightness}}}"#.to_string(),
+            init_command: None,
+            topic: "lights/light_1/set".to_string(),
+            on: "ON".to_string(),
+            off: "OFF".to_string(),
+        };
+        let (topic, command) = switch_command.get_topic_and_command(SwitchState::On, 123);
+        assert_eq!(topic, "lights/light_1/set");
+        assert_eq!(command, r#"{"state":"ON","brightness":123}"#);
+        let (topic, command) = switch_command.get_topic_and_command(SwitchState::Off, 123);
+        assert_eq!(topic, "lights/light_1/set");
+        assert_eq!(command, r#"{"state":"OFF","brightness":123}"#);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,19 @@ mod configuration;
 mod dummy_configuration;
 mod mqtt;
 
+use crate::configuration::Configuration;
 use crate::dummy_configuration::hardcoded_config;
 use paho_mqtt::MessageBuilder;
-use serde_json::Value;
+use std::sync::mpsc;
+use std::sync::mpsc::{Receiver, Sender};
+use std::thread;
+use std::time::Duration;
 
 fn main() {
     let mut configuration = hardcoded_config();
     let topics_to_subscribe = configuration.get_topics();
 
+    // connect and subscribe to mqtt
     let mut mqtt_client = MqttClient::new(
         "tcp://pepe.private:1883".to_string(),
         "homeassistant".to_string(),
@@ -23,6 +28,7 @@ fn main() {
         topics_to_subscribe,
     );
 
+    // trigger status updates for devices
     for switch in configuration.switches.iter() {
         if switch.command.init_command.is_none() {
             continue;
@@ -36,20 +42,50 @@ fn main() {
         mqtt_client.cli.publish(message);
     }
 
-    let receiver = mqtt_client.cli.start_consuming();
-    for msg in receiver.iter() {
-        if let Some(msg) = msg {
-            let topic = msg.topic();
-            let payload_str = msg.payload_str();
+    // create thread channels
+    let (update_sender, update_receiver): (Sender<UpdateMessage>, Receiver<UpdateMessage>) =
+        mpsc::channel();
+    let ping_sender = update_sender.clone();
 
-            match serde_json::from_str(&payload_str) {
-                Result::Ok(payload) => {
-                    configuration.update_sensor_for_topic(topic, &payload);
-                    configuration.update_switch_for_topic(topic, &payload);
-                    configuration.print_room_state();
+    // start thread which reacts on state changes
+    let mqtt_receiver = mqtt_client.cli.start_consuming();
+    thread::spawn(move || {
+        for msg in mqtt_receiver.iter() {
+            if let Some(msg) = msg {
+                let topic = msg.topic();
+                let payload_str = msg.payload_str();
+
+                match serde_json::from_str(&payload_str) {
+                    Result::Ok(payload) => {
+                        configuration.update_sensor_for_topic(topic, &payload);
+                        configuration.update_switch_for_topic(topic, &payload);
+                        update_sender.send(UpdateMessage::StateChange(configuration.clone()));
+                    }
+                    _ => {}
                 }
-                _ => {}
             }
         }
+    });
+
+    // start thread that triggers regular ping messages
+    thread::spawn(move || loop {
+        thread::sleep(Duration::from_millis(1000));
+        ping_sender.send(UpdateMessage::Ping);
+    });
+
+    // main loop
+    for update_message in update_receiver.iter() {
+        match update_message {
+            UpdateMessage::Ping => println!("got ping"),
+            UpdateMessage::StateChange(_) => println!("got state change"),
+        }
     }
+}
+
+/// Object used to send messages to the main decision engine
+pub enum UpdateMessage {
+    /// Send a State change
+    StateChange(Configuration),
+    // used to trigger regular calculation
+    Ping,
 }
